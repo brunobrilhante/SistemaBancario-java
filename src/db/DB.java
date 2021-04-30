a2e9c2639c3e8e9336abaee4d9e8113cf70e3617
Bruno Brilhante <brunobrilhante53@gmail.com>
2021-04-30 13:12:27 -0300

    Adicionando as propriedades para o JDBC

---
+++ src/db/DB.java
@@ -0,0 +1,15 @@
+package db;
+
+import java.util.Properties;
+
+/**
+ *
+ * @author Bruno
+ */
+public class DB {
+    
+    private static Properties loadProperties(){
+        
+    }
+    
+}
---
+++ src/db/DbException.java
@@ -0,0 +1,12 @@
+package db;
+
+/**
+ *
+ * @author Bruno
+ */
+public class DbException extends RuntimeException{
+    
+    public DbException(String msg){
+        super(msg);
+    }
+}
